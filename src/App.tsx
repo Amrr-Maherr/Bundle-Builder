c4cc99a9f2a3399d4c4a3b082dac7d13cffaffb6
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-07-03 23:31:07 +0300

    feat: add SVG assets for React and Vite, implement Accordion and Button components, and set up TypeScript configuration
    - Added react.svg and vite.svg to src/assets
    - Created Accordion component with AccordionItem, AccordionTrigger, and AccordionContent
    - Developed Button component with variant and size support
    - Established utility function for class name merging in utils.ts
    - Initialized main entry point in main.tsx
    - Configured TypeScript settings in tsconfig files for app and node
    - Set up Vite configuration with React and Tailwind CSS plugins
    - Added global styles in index.css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button className="bg-red-600">test</Button>
+    </>
+  );
+}
+
+export default App;
